413070413d36e00d446b20e6ccd51f54715f1c0b
Nate Berkopec <nate.berkopec@gmail.com>
2016-05-03 09:14:06 -0400

    Inline production.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,21 @@ module Tweetlength
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.cache_classes = true
+    config.eager_load = true
+    config.consider_all_requests_local       = false
+    config.action_controller.perform_caching = true
+    config.log_level = :debug
+    config.log_tags = [ :request_id ]
+    config.i18n.fallbacks = true
+    config.active_support.deprecation = :notify
+    config.log_formatter = ::Logger::Formatter.new
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
+    
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     routes.draw do
