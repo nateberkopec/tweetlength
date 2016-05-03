826f7d4e971ace9840a3a92fdda3b6f3104875ab
Nate Berkopec <nate.berkopec@gmail.com>
2016-05-03 09:09:05 -0400

    Inline secrets.yml
    Our objective for the next few commits will be to inline all the remaining files into one.
    
    secrets.yml is just a Rails-sanctioned YAML file for managing the secret key config. We can also set this with `config.secret_key_base`.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Tweetlength
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
 
